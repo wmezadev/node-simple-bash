930d7c632a2be0bfa3630ebb7f1ff142b6336006
William Eduardo Meza <williameduardomeza@gmail.com>
2020-07-20 20:40:18 -0500

    feat: add commands

--- bash.js
+++ bash.js
@@ -1,7 +1,26 @@
 const commands = require('./commands/index.js');
 
-process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim();
-  commands[cmd]()
-  process.stdout.write('\nprompt > ');
+function validateCommand(command){
+    const arr = Object.keys(commands);
+    return arr.findIndex(element => element === command) >= 0; 
+}
+
+ // Output un prompt
+ process.stdout.write('prompt > ');
+ // El evento stdin 'data' se dispara cuando el user escribe una línea
+ process.stdin.on('data', function (data) {
+   let cmd = data.toString().trim(); // remueve la nueva línea
+
+   const cmdList = cmd.split(" ");
+   let firstCmd =  cmdList[0];
+   cmdList.shift();
+   let params = cmdList.join(" ");
+
+   if(validateCommand(firstCmd)) {
+       commands[firstCmd](params);
+   } else {
+       process.stdout.write("Commando inválido. \n");
+       process.stdout.write('prompt > ');
+   }
+
 });
--- commands/index.js
+++ commands/index.js
@@ -1,8 +1,29 @@
-module.export = {
-    date: function () {
-        process.stdout.write(Date)();
+fs = require('fs');
+
+module.exports = {
+    date: function() {
+        process.stdout.write(Date());
+        process.stdout.write("\nprompt > ");
     },
     pwd: function() {
         process.stdout.write(process.env.PWD);
+        process.stdout.write("\nprompt > ");
+    },
+    ls: function() {
+        fs.readdir('.', function(err, files) {
+            if (err) throw err;
+            files.forEach(function(file) {
+              process.stdout.write(file.toString() + "\n");
+            })
+            process.stdout.write("prompt > ");
+        });
+    },
+    echo: function (data) {
+        if(data){
+            process.stdout.write(data);
+        } else {
+            process.stdout.write("nada que imprimir");
+        }
+        process.stdout.write("\nprompt > ");
     }
 }
